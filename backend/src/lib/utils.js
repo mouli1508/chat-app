68058367fcffd575066030ddac23243d5263c1a5
mouli1508 <chandramouli.p1508@gmail.com>
2025-09-22 18:52:43 -0400

    updated changes based on code rabbit

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -15,7 +15,7 @@ export const generateToken = (userId, res) => {
         httpOnly: true,
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         sameSite: "strict",
-        seccure: ENV.NODE_ENV === "development" ? false : true,
+        secure: ENV.NODE_ENV === "development" ? false : true,
     });
 
     return token;
